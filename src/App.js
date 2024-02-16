a2b23790fa4c8143199de0e84a42af9f2a817081
khang2000 <khanghtqt70@gmail.com>
2024-02-16 14:43:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faPen } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, useRef, useEffect } from "react";
 import { addCustomer, uploadImage } from "./app.service";
-
+import { newData } from "./constant";
 function App() {
   const apiUrl = "https://g.lifetek.vn:203/api/files/single";
   const customerApiUrl = "https://g.lifetek.vn:220/api/customers";
@@ -12,83 +12,11 @@ function App() {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjI2NjBhYzFjOGM4MTMwNmYxODQ5NTIwIiwiaWF0IjoxNzA2NTgzMjI5LCJleHAiOjE3MTg1ODMyMjl9.pZS9oGDy3ftrN9-fWWP8JVnG7cHfibHOKtw3UgaFMDI";
   const [image, setImage] = useState();
   const inputRef = useRef(null);
-  // useEffect(() => {
-  //   return () => {
-  //     image && URL.revokeObjectURL(URL.createObjectURL(image));
-  //   };
-  // }, [image]);
+
   const handleImageClick = () => {
     inputRef.current.click();
   };
-  const newData = {
-    type: 1,
-    kanbanStatus: "62c63d566b24ef5278427ef0",
-    others: {},
-    callback: false,
-    facebook: "",
-    code: "KH1706603392670",
-    name: "A",
-    lastName: "Nguy?n Van",
-    nickName: "NVA",
-    phoneNumber: "09422135487",
-    email: "",
-    gender: "male",
-    birthDay: "2024-01-30",
-    provincial: null,
-    website: "Website A",
-    fax: "020321321321",
-    idetityCardNumber: "876543",
-    passportNumber: "sdfghjkl",
-    bank: "nganhang3",
-    bankAccountNumber: "0987654342",
-    taxCode: "",
-    isTax: false,
-    address: "H� N?i",
-    certifiedNoTaxNumber: "",
-    position: "",
-    businessRegistrationNumber: "",
-    managerEmployee: null,
-    viewableEmployees: [],
-    note: "",
-    avatar: "https://g.lifetek.vn:203/api/files/65b8b3fdbca66b6de2f44c0b",
-    avatarURL:
-      "blob:http://localhost:2002/c3b68d93-c333-47e8-8bc8-f3e520f3c70f",
-    detailInfo: {
-      typeCustomer: {
-        typeOfCustomer: "c1",
-        group: "",
-        branches: "",
-        career: [],
-        productType: [],
-        contactWays: "",
-        areas: [],
-        introducedWay: "",
-        introPerson: "",
-        phoneIntroPerson: "",
-        introducedNote: "",
-        setAttribute: {},
-      },
-      represent: {
-        name: "",
-        phoneNumber: "",
-        gender: "male",
-        birthDay: "30/01/2024",
-        email: "",
-        position: "",
-        note: "",
-        localPersonInfo: [],
-      },
-      options: {
-        isTaxTitle: false,
-        taxTitle: [
-          {
-            name: "bac",
-            percent: 10,
-          },
-        ],
-      },
-    },
-  };
+
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     console.log(file);
@@ -110,41 +38,7 @@ function App() {
     passportNumber: "",
     bankAccountNumber: "",
   });
-  // const formData = new FormData();
-  // formData.append("file", image);
-  // const Upload = (e) => {
-  //   e.preventDefault();
 
-  //   fetch(`https://g.lifetek.vn:203/api/files/single`, {
-  //     method: "POST",
-
-  //     body: formData,
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data);
-
-  //       alert(data.message);
-  //     })
-  //     .catch((error) => console.log(error.message));
-  // };
-
-  // const Upload = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const data = await uploadImage(apiUrl, image);
-  //     console.log(data);
-  //     if (data.url) {
-  //       newData.avatar = data.url;
-  //       newData.avatarURL = URL.createObjectURL(image);
-  //       console.log(newData);
-  //     } else {
-  //       newData.avatar = "";
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
   const getData = (e) => {
     const { value, name } = e.target;
     setInpVal(() => {
@@ -156,17 +50,6 @@ function App() {
   };
   const addData = async (e) => {
     e.preventDefault();
-    // const { name, nickname, phoneNumber, birthDay } = inpVal;
-    // if (name === "") {
-    //   alert("fill your name");
-    // } else if (nickname === "") {
-    //   alert("fill your nickname");
-    // } else if (phoneNumber === "") {
-    //   alert("fill your phoneNumber");
-    // } else if (birthDay === "") {
-    //   alert("fill your birthDay");
-    // } else {
-    // const data = { name, nickname, phoneNumber, birthDay, };
 
     newData.name = inpVal.name;
     newData.nickname = inpVal.nickname;
@@ -208,23 +91,6 @@ function App() {
     } catch (error) {
       console.error(error);
     }
-
-    // fetch("https://g.lifetek.vn:220/api/customers", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjI2NjBhYzFjOGM4MTMwNmYxODQ5NTIwIiwiaWF0IjoxNzA2NTgzMjI5LCJleHAiOjE3MTg1ODMyMjl9.pZS9oGDy3ftrN9-fWWP8JVnG7cHfibHOKtw3UgaFMDI`,
-    //   },
-    //   body: JSON.stringify(newData),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-
-    //     alert(data.message);
-    //   })
-    //   .catch((error) => console.log(error.message));
-    // }
   };
   return (
     <div className="body-ct">
